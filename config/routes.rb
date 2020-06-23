d5778930f1706e462170b7927184f64ae3c0c237
venturian123 <milly@venturit.com>
2020-06-23 18:52:05 -0400

    Fixed search route

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Home
   root 'home#index'
-  
-  get 'users/:id', to: 'users#show', as: 'user'
   get 'users/search', to: 'users#search'
+  get 'users/:id', to: 'users#show', as: 'user'
+
   
 
 end
